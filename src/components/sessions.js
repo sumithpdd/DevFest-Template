ae28692e730f9c08db60bcb4fca2670ef322d1db
christosbouloumpasis <christosbouloumpasis@gmail.com>
2023-11-25 22:44:41 +0000

    multiple speakers

--- src/components/sessions.js
+++ src/components/sessions.js
@@ -7,7 +7,7 @@ const Sessions = () => {
         fetch('https://sessionize.com/api/v2/fvji6esg/view/Sessions')
             .then(response => response.json())
             .then(data => {
-                if(data && data[0] && data[0].sessions) {
+                if (data && data[0] && data[0].sessions) {
                     setSessions(data[0].sessions);
                 }
             });
@@ -19,28 +19,30 @@ const Sessions = () => {
 
     return (
         <section className="text-center bg-LightBlue py-10">
-
             <h2 className="text-center">Sessions</h2>
             <div className='w-11/12 mx-auto'>
                 {sessions.length > 0 && sessions.map((session, idx) => (
                     <div className='session-component my-10 p-5' key={idx}>
                         <h3 className='mb-3'>{session.title}</h3>
                         {session.speakers && session.speakers.length > 0 && (
-                            <p className='font-semibold'>Speaker: {session.speakers[0].name}</p>
+                            <p className='font-semibold'>
+                                Speakers: {session.speakers.map((speaker, speakerIdx) => (
+                                <span key={speakerIdx}>
+                                        {speaker.name}{speakerIdx < session.speakers.length - 1 ? ', ' : ''}
+                                    </span>
+                            ))}
+                            </p>
                         )}
                         <p className='mb-3'>{session.description}</p>
-
                         {session.categories.map(category =>
-                                category.categoryItems.map(item => {
-                                    // Determine the class based on the item name
-                                    const pillClass = item.name.includes("Talk") ? "talk-pill" : "workshop-pill";
-
-                                    return (
-                                        <span className={pillClass}>
-                                            {item.name.includes("Talk") ? "Talk" : "Workshop"}
-                                        </span>
-                                    );
-                                })
+                            category.categoryItems.map((item, itemIdx) => {
+                                const pillClass = item.name.includes("Talk") ? "talk-pill" : "workshop-pill";
+                                return (
+                                    <span key={itemIdx} className={pillClass}>
+                                        {item.name.includes("Talk") ? "Talk" : "Workshop"}
+                                    </span>
+                                );
+                            })
                         )}
                     </div>
                 ))}
@@ -50,4 +52,3 @@ const Sessions = () => {
 };
 
 export default Sessions;
-
